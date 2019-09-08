513ff79aa55b395536a95d26f0b7c6441ffe96cb
alejandroesalas <alejandroesalas@gmail.com>
2019-09-08 05:35:17 -0500

    Agregadas configuraciones necesarias para el deploy de la aplicacion en heroku

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,73 +1,72 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import { AdminComponent } from './admin.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
-import { AdminHomeComponent } from './components/admin-home/admin-home.component';
-import { DisabledSectionComponent } from './components/disabled-section/disabled-section.component';
-import {AdminRoutingModule} from "./admin-routing.module";
-import { AdminSectionComponent } from './components/admin-section/admin-section.component';
-import {AuthService} from "../services/authService/auth.service";
-import {_adminGuard} from "../guards/_admin.guard";
-import {StudentService} from "../services/student/student.service";
-import {ProgramService} from "../services/program/program.service";
-import {FacultyService} from "../services/faculty/faculty.service";
-import {CoordinatorService} from "../services/coodinator/coordinator.service";
-import { FacultiesComponent } from './components/faculties/faculties.component';
-import { FacultiesEditComponent } from './components/faculties-edit/faculties-edit.component';
-import { UsersComponent } from './components/users/users.component';
-import { ProgramsComponent } from './components/programs/programs.component';
-import {ModalModule} from "../_modal";
-import { UsersEditComponent } from './components/users-edit/users-edit.component';
-import { UsersAddComponent } from './components/users-add/users-add.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AdminComponent} from './admin.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {AdminHomeComponent} from './components/admin-home/admin-home.component';
+import {DisabledSectionComponent} from './components/disabled-section/disabled-section.component';
+import {AdminRoutingModule} from './admin-routing.module';
+import {AdminSectionComponent} from './components/admin-section/admin-section.component';
+import {AuthService} from '../services/authService/auth.service';
+import {_adminGuard} from '../guards/_admin.guard';
+import {StudentService} from '../services/student/student.service';
+import {ProgramService} from '../services/program/program.service';
+import {FacultyService} from '../services/faculty/faculty.service';
+import {CoordinatorService} from '../services/coodinator/coordinator.service';
+import {FacultiesComponent} from './components/faculties/faculties.component';
+import {FacultiesEditComponent} from './components/faculties-edit/faculties-edit.component';
+import {UsersComponent} from './components/users/users.component';
+import {ProgramsComponent} from './components/programs/programs.component';
+import {ModalModule} from '../_modal';
+import {UsersEditComponent} from './components/users-edit/users-edit.component';
+import {UsersAddComponent} from './components/users-add/users-add.component';
 import {
-  MatInputModule,
-  MatProgressSpinnerModule,
-  MatSelectModule,
-  MatSnackBar,
-  MatSnackBarModule
-} from "@angular/material";
-import { ProgramsAddComponent } from './components/programs-add/programs-add.component';
-import { ProgramsEditComponent } from './components/programs-edit/programs-edit.component';
-import { CoordinatorsHomeComponent } from './components/coordinators-home/coordinators-home.component';
-import { CoordinatorsAddComponent } from './components/coordinators-add/coordinators-add.component';
-import { CoordinatorsAditComponent } from './components/coordinators-adit/coordinators-adit.component';
-import { DisabledSectionBodyComponent } from './components/disabled-section/disabled-section-body/disabled-section-body.component';
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSnackBar,
+    MatSnackBarModule
+} from '@angular/material';
+import {ProgramsAddComponent} from './components/programs-add/programs-add.component';
+import {ProgramsEditComponent} from './components/programs-edit/programs-edit.component';
+import {CoordinatorsHomeComponent} from './components/coordinators-home/coordinators-home.component';
+import {CoordinatorsAddComponent} from './components/coordinators-add/coordinators-add.component';
+import {CoordinatorsAditComponent} from './components/coordinators-adit/coordinators-adit.component';
+import {DisabledSectionBodyComponent} from './components/disabled-section/disabled-section-body/disabled-section-body.component';
 
 
 @NgModule({
-  declarations:[
-    AdminComponent,
-    AdminSectionComponent,
-    AdminHomeComponent,
-    DisabledSectionComponent,
-    FacultiesComponent,
-    FacultiesEditComponent,
-    UsersComponent,
-    ProgramsComponent,
-    UsersEditComponent,
-    UsersAddComponent,
-    ProgramsAddComponent,
-    ProgramsEditComponent,
-    CoordinatorsHomeComponent,
-    CoordinatorsAddComponent,
-    CoordinatorsAditComponent,
-    DisabledSectionBodyComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    ModalModule,
-    MatSelectModule,
-    MatInputModule,
-    AdminRoutingModule,
-    MatProgressSpinnerModule,
-    MatSnackBarModule
-  ],
-  exports:[
-  ],
-  providers:[MatSnackBar,AuthService,_adminGuard,StudentService,ProgramService,FacultyService,CoordinatorService]
+    declarations: [
+        AdminComponent,
+        AdminSectionComponent,
+        AdminHomeComponent,
+        DisabledSectionComponent,
+        FacultiesComponent,
+        FacultiesEditComponent,
+        UsersComponent,
+        ProgramsComponent,
+        UsersEditComponent,
+        UsersAddComponent,
+        ProgramsAddComponent,
+        ProgramsEditComponent,
+        CoordinatorsHomeComponent,
+        CoordinatorsAddComponent,
+        CoordinatorsAditComponent,
+        DisabledSectionBodyComponent],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        ModalModule,
+        MatSelectModule,
+        MatInputModule,
+        AdminRoutingModule,
+        MatProgressSpinnerModule,
+        MatSnackBarModule
+    ],
+    exports: [],
+    providers: [MatSnackBar, AuthService, _adminGuard, StudentService, ProgramService, FacultyService, CoordinatorService]
 })
 export class AdminModule {
 
--- src/app/global.ts
+++ src/app/global.ts
@@ -1,20 +1,20 @@
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
 
-export var global = {
-  url :'http://localhost/SIGPQR/SIGPQR-Back-End/public/api/',
-  contentType:'application/x-www-form-urlencoded',
-  tagProgram:'programs',
-  tagAdmin:'admin',
-  tagUser:'users',
-  tagCoordinator:'coordinators',
-  tagFaculty:'faculties',
-  tagAdd:'add',
-  tagEdit:'edit'
-}
+export const global = {
+    url: 'http://localhost/SIGPQR-Back-End/public/api/',
+    contentType: 'application/x-www-form-urlencoded',
+    tagProgram: 'programs',
+    tagAdmin: 'admin',
+    tagUser: 'users',
+    tagCoordinator: 'coordinators',
+    tagFaculty: 'faculties',
+    tagAdd: 'add',
+    tagEdit: 'edit'
+};
 export const swal = Swal.mixin({
-  customClass: {
-    confirmButton: 'btn btn-success',
-    cancelButton: 'btn btn-danger'
-  },
-  buttonsStyling: false
-})
+    customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+});
